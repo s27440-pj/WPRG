ffab215c9d9bebbfc7f0dd1961e1463bb3df1a8d
krysiatokarska <s27440@pjwstk.edu.pl>
2024-05-06 15:20:08 +0200

    zad2 update

--- Zadania4/zad2.php
+++ Zadania4/zad2.php
@@ -2,9 +2,17 @@
     $file_path = './plik2.txt';
     if (file_exists($file_path)) {
         $visits = file_get_contents($file_path);
-        $visits++;
+        if ($visits !== false) {
+            $visits++;
+        } else {
+            echo "Nie można otworzyć pliku";
+        }
     } else {
         $visits = 1;
     }
-    file_put_contents($file_path, $visits);
+    if(file_put_contents($file_path, $visits) !== FALSE) {
+        echo "Zapis do pliku udany. Liczba odwiedzin strony: " . $visits;
+    } else {
+        echo "Błąd zapisu, spróbuj ponownie";
+    }
 ?>
